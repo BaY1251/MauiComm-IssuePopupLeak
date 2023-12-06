ed8ceb890f8a29fd48bcbef6655a3d3736e81fd5
BaY <BaY>
2023-12-06 15:36:08 +0800

    Popup

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,44 +2,44 @@
 
 namespace MauiComm_IssuePopupLeak
 {
-    public partial class MainPage : ContentPage
-    {
-        int maxShowCnt = int.MaxValue;
-        int showCnt = 0;
-        bool isShow = false;
+	public partial class MainPage : ContentPage
+	{
+		int showCnt = 0;
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+		public MainPage()
+		{
+			InitializeComponent();
+		}
 
-        void btnShow_Clicked(object sender, EventArgs e)
-        {
-            var timer = Dispatcher.CreateTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(250);
-            showCnt = 0;
-            PopupPage1 popup = null;
-            timer.Tick += async (s, e) =>
-            {
-                if (showCnt == maxShowCnt)
-                {
-                    timer.Stop();
-                }
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+		}
 
-                if (!isShow)
-                {
-                    popup = new PopupPage1();
-                    this.ShowPopup(popup);
-                    isShow = true;
-                }
-                else
-                {
-                    await popup.CloseAsync();
-                    isShow = false;
-                    showCnt++;
-                }
-            };
-            timer.Start();            
-        }
-    }
+		void btnShow_Clicked(object sender, EventArgs e)
+		{
+			(sender as View).IsVisible = false;
+			var timer = Dispatcher.CreateTimer();
+			timer.Interval = TimeSpan.FromMilliseconds(250);
+			showCnt = 0;
+			PopupPage1 popup = null;
+			timer.Tick += delegate
+			{
+				timer.Stop();
+				popup = new PopupPage1();
+				popup.Opened += delegate
+				{
+					popup.Update(showCnt.ToString());
+					Dispatcher.DispatchDelayed(TimeSpan.FromMilliseconds(250), async delegate
+					{
+						await popup.CloseAsync();
+					});
+				};
+				popup.Closed += delegate { timer.Start(); };
+				showCnt++;
+				this.ShowPopup(popup);
+			};
+			timer.Start();
+		}
+	}
 }
--- PopupPage1.xaml.cs
+++ PopupPage1.xaml.cs
@@ -8,4 +8,6 @@ public partial class PopupPage1 : Popup
 	{
 		InitializeComponent();
 	}
+
+	public void Update(string msg) => L_Count.Text = msg;
 }
